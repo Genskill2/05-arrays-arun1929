7b552da44429183be71031aa6f94a19ece214577
arun1929 <84227806+arun1929@users.noreply.github.com>
2021-06-16 10:27:12 -0400

    Add files via upload

--- solns.c
+++ solns.c
@@ -1,5 +1,6 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+//max
  int max(int m[],int n)
   {
   	int max=0;
@@ -13,3 +14,31 @@
   	return max;
   }
  
+ //min
+ int min(int m[],int n)
+  {
+  	int min=0;
+  	for(int i=0;i<n;i++)
+  	{
+  		if(m[i]>min)
+  			min=m[i];
+  		else 
+  			continue;
+  	}
+  	return min;
+  }
+  
+  //average
+   float average(int a[],int n)
+  {
+  	float average=0;
+  	int i;
+  	for(i=0;i<n;i++)
+  	{
+  		if(a[i]/2==0)
+  			average=a[i];
+  		else 
+  			continue;
+  	}
+  	return average;
+  }
