28235b6c91899108befc088d4f8ca9df61fdd547
arun1929 <84227806+arun1929@users.noreply.github.com>
2021-06-16 10:24:19 -0400

    Add files via upload

--- solns.c
+++ solns.c
@@ -1,2 +1,15 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+ int max(int m[],int n)
+  {
+  	int max=0;
+  	for(int i=0;i<n;i++)
+  	{
+  		if(m[i]>max)
+  			max=m[i];
+  		else 
+  			continue;
+  	}
+  	return max;
+  }
+ 
